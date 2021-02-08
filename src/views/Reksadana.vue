61273fd5ea5da91db2436b58f244e10baed54230
Bimo <bagus.b@mail.ugm.ac.id>
2021-02-08 20:55:10 +0700

    rework reksadana page

--- src/views/Reksadana.vue
+++ src/views/Reksadana.vue
@@ -3,86 +3,30 @@
     <div
       class="max-w-screen-xl px-4 pt-4 mx-auto sm:px-6 lg:pt-8 lg:px-8 lg:items-center lg:justify-between"
     >
-        <div class="flex rounded-md">
-            <button class="bg-gray-200 rounded-md w-auto flex justify-end items-center text-blue-500 p-3 hover:text-blue-400">
-               <svg version="1.1" class="h-4 stroke-current text-dark" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-                    viewBox="0 0 52.966 52.966" style="enable-background:new 0 0 52.966 52.966;" xml:space="preserve">
-                        <path d="M51.704,51.273L36.845,35.82c3.79-3.801,6.138-9.041,6.138-14.82c0-11.58-9.42-21-21-21s-21,9.42-21,21s9.42,21,21,21
-                        c5.083,0,9.748-1.817,13.384-4.832l14.895,15.491c0.196,0.205,0.458,0.307,0.721,0.307c0.25,0,0.499-0.093,0.693-0.279
-                        C52.074,52.304,52.086,51.671,51.704,51.273z M21.983,40c-10.477,0-19-8.523-19-19s8.523-19,19-19s19,8.523,19,19
-                        S32.459,40,21.983,40z"/>
-                </svg>
-            </button>
-            <input class="w-full border rounded p-2 mx-2" type="text" placeholder="Search...">
-            <div class="relative inline-block text-left">
-                <div>
-                    <button
-                        type="button"
-                        class="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500"
-                        id="options-menu"
-                        aria-haspopup="true"
-                        aria-expanded="true"
-                        @click="isOpen = !isOpen"
-                    >
-                        SORT
-                        <svg v-if="!isOpen" class="-mr-1 ml-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                        </svg>
-                        <svg v-else class="-mr-1 ml-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                            <path fill-rule="evenodd" d="M14.707 12.707a1 1 0 01-1.414 0L10 9.414l-3.293 3.293a1 1 0 01-1.414-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 010 1.414z" clip-rule="evenodd" />
-                        </svg>
-                    </button>
-                </div>
-                <transition
-                    enter-active-class="transition-opacity duration-300 ease-linear"
-                    enter-from-class="opacity-0"
-                    enter-to-class="opacity-100"
-                    leave-active-class="transition-opacity duration-300 ease-linear"
-                    leave-from-class="opacity-100"
-                    leave-to-class="opacity-0"
-                >
-                    <div v-show="isOpen" class="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
-                        <div class="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
-                            <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">Account settings</a>
-                            <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">Support</a>
-                            <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">License</a>
-                            <form method="POST" action="#">
-                                <button
-                                    type="submit"
-                                    class="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900"
-                                    role="menuitem"
-                                >
-                                    Sign out
-                                </button>
-                            </form>
-                        </div>
-                    </div>
-                </transition>
-            </div>
-        </div>
-        <table class="min-w-full divide-y divide-gray-200 mt-4">
-            <thead class="bg-gray-50">
-                <tr>
-                    <th scope="col" class="px-6 py-3 text-left text-xs font-extrabold text-gray-500 uppercase tracking-wider">
+        <SearchComponent :data="reksadana" :isOpen="isOpen" :searchFunction="searchFilter" :filterFunction="selectFilter" :filterStringArray="filterArray" />
+        <table class="text-left w-full divide-y divide-gray-200 mt-4">
+            <thead class="bg-gray-50 flex text-white w-full">
+                <tr class="flex w-full mb-4">
+                    <th scope="col" class="px-4 pt-4 w-1/3 text-left text-xs font-extrabold text-gray-500 uppercase tracking-wider">
                         Name
                     </th>
-                    <th scope="col" class="px-6 py-3 text-left text-xs font-extrabold text-gray-500 uppercase tracking-wider">
+                    <th scope="col" class="px-4 pt-4 w-1/3 text-left text-xs font-extrabold text-gray-500 uppercase tracking-wider">
                         Amount
                     </th>
-                    <th scope="col" class="px-6 py-3 text-left text-xs font-extrabold text-gray-500 uppercase tracking-wider">
+                    <th scope="col" class="px-4 pt-4 w-1/3 text-left text-xs font-extrabold text-gray-500 uppercase tracking-wider">
                         Return
                     </th>
                 </tr>
             </thead>
-            <tbody v-for="(content, i) in reksadana.contents" :key="i" class="bg-white divide-y divide-gray-200">
-                <tr>
-                    <td class="px-6 py-4 whitespace-nowrap">
+            <tbody class="bg-white flex flex-col items-center justify-between divide-y overflow-y-scroll h-72 w-full">
+                <tr v-for="(content, i) in reksadana" :key="i" class="flex w-full mb-4">
+                    <td class="px-4 pt-4 w-1/3 whitespace-nowrap">
                         {{ content.name }}
                     </td>
-                    <td class="px-6 py-4 whitespace-nowrap font-bold">
+                    <td class="px-4 pt-4 w-1/3 whitespace-nowrap font-bold">
                         Rp {{ Number(content.amount).toLocaleString('id-ID') }},00
                     </td>
-                    <td class="px-6 py-4 whitespace-nowrap">
+                    <td class="px-4 pt-4 w-1/3 whitespace-nowrap">
                         {{ content.return }}
                     </td>
                 </tr>
@@ -93,18 +37,76 @@
 </template>
 
 <script>
-import TableComponent from '@/components/TableComponent.vue'
+import SearchComponent from '@/components/SearchComponent.vue'
 import Reksadana from '@/data/reksadana/reksadana.json'
 
 export default {
-  components: { TableComponent },
+  components: { SearchComponent },
   data() {
     return {
       isOpen: false,
-      reksadana: Reksadana,
+      reksadana: [],
+      filterArray: [
+          {
+              name: 'All',
+              keyword: '',
+          },
+          {
+              name: 'Positif',
+              keyword: 'positive',
+          },
+          {
+              name: 'Negatif',
+              keyword: 'negative',
+          },
+      ],
     };
   },
 
-  methods: {}
+  mounted() {
+    for (let i = 0; i < Reksadana.contents.length; i+=1) {
+        this.reksadana.push(Reksadana.contents[i])
+    }
+  },
+
+  methods: {
+    searchFilter(e) {
+      if (e.target.value == '') {
+        this.reksadana = Reksadana.contents
+      } else {       
+        this.reksadana = this.reksadana.filter(inv => {
+            return inv.name.toLowerCase().includes(e.target.value.toLowerCase())
+        })
+      }
+    },
+    selectFilter(keyword) {
+        if (keyword == '') {
+            let result = this.reksadana
+                .sort((a, b) => {
+                    if (a.name < b.name) {
+                        return -1
+                    }
+                    if (a.name > b.name) {
+                        return 1
+                    }
+                    return 0;
+                })
+        } else {
+            if (keyword == 'positive') {  
+                let result = this.reksadana
+                    .filter(o => o.return >= 0)
+                    .sort((a, b) => a.return >= 0 - b.return >= 0)            
+                let difference = this.reksadanas.filter(x => !result.includes(x))
+                this.reksadana = result.concat(difference)
+            } else {              
+                let result = this.reksadana
+                    .filter(o => o.return < 0)
+                    .sort((a, b) => a.return >= 0 - b.return >= 0)     
+                let difference = this.reksadana.filter(x => !result.includes(x))
+                this.reksadana = result.concat(difference)
+            }
+        }
+    }
+  }
 }
 </script>
