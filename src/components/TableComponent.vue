172428ef71f385b251f9e6c2d72fa27cf4ee0e6b
Bimo <bagus.b@mail.ugm.ac.id>
2021-02-08 20:54:39 +0700

    rework table component

--- src/components/TableComponent.vue
+++ src/components/TableComponent.vue
@@ -1,41 +1,41 @@
 <template>
-<table class="min-w-full divide-y divide-gray-200 mt-4">
-    <thead class="bg-gray-50">
-        <tr>
-            <th scope="col" class="px-6 py-3 text-left text-xs font-extrabold text-gray-500 uppercase tracking-wider">
+<table class="text-left w-full divide-y divide-gray-200 mt-4">
+    <thead class="bg-gray-50 flex text-white w-full">
+        <tr class="flex w-full mb-4">
+            <th scope="col" class="px-4 pt-4 w-1/5 text-left text-xs font-extrabold text-gray-500 uppercase tracking-wider">
                 Name
             </th>
-            <th scope="col" class="px-6 py-3 text-left text-xs font-extrabold text-gray-500 uppercase tracking-wider">
+            <th scope="col" class="px-4 pt-4 w-1/5 text-left text-xs font-extrabold text-gray-500 uppercase tracking-wider">
                 Amount
             </th>
-            <th scope="col" class="px-6 py-3 text-left text-xs font-extrabold text-gray-500 uppercase tracking-wider">
+            <th scope="col" class="px-4 pt-4 w-1/5 text-left text-xs font-extrabold text-gray-500 uppercase tracking-wider">
                 Tenor
             </th>
-            <th scope="col" class="px-6 py-3 text-left text-xs font-extrabold text-gray-500 uppercase tracking-wider">
+            <th scope="col" class="px-4 pt-4 w-1/5 text-left text-xs font-extrabold text-gray-500 uppercase tracking-wider">
                 Grade
             </th>
-            <th scope="col" class="px-6 py-3 text-left text-xs font-extrabold text-gray-500 uppercase tracking-wider">
+            <th scope="col" class="px-4 pt-4 w-1/5 text-left text-xs font-extrabold text-gray-500 uppercase tracking-wider">
                 Rate
             </th>
         </tr>
     </thead>
-    <tbody v-for="(content, i) in data.contents" :key="i" class="bg-white divide-y divide-gray-200">
-        <tr>
-        <td class="px-6 py-4 whitespace-nowrap">
-            {{ content.name }}
-        </td>
-        <td class="px-6 py-4 whitespace-nowrap font-bold">
-            Rp {{ Number(content.amount).toLocaleString('id-ID') }},00
-        </td>
-        <td class="px-6 py-4 whitespace-nowrap">
-            {{ content.tenor }}
-        </td>
-        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-            {{ content.grade }}
-        </td>
-        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-            {{ content.rate }}
-        </td>
+    <tbody class="bg-white flex flex-col items-center justify-between divide-y overflow-y-scroll h-72 w-full">
+        <tr v-for="(content, i) in data" :key="i" class="flex w-full mb-4">
+            <td class="px-4 pt-4 w-1/5 whitespace-nowrap">
+                {{ content.name }}
+            </td>
+            <td class="px-4 pt-4 w-1/5 whitespace-nowrap font-bold">
+                Rp {{ Number(content.amount).toLocaleString('id-ID') }},00
+            </td>
+            <td class="px-4 pt-4 w-1/5 whitespace-nowrap">
+                {{ content.tenor }}
+            </td>
+            <td class="px-4 pt-4 w-1/5 whitespace-nowrap text-sm text-gray-500">
+                {{ content.grade }}
+            </td>
+            <td class="px-4 pt-4 w-1/5 whitespace-nowrap text-sm text-gray-500">
+                {{ content.rate }}
+            </td>
         </tr>
     </tbody>
 </table>
@@ -45,7 +45,7 @@
 export default {
   name: 'TableComponent',
   props: {
-    data: Array,
+    data: Object,
   },
 }
 </script>
